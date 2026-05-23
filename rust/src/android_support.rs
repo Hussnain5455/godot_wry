be8e7fe7dab535869efd095040242cde6c1318e6
Hussnain5455 <hussnain5455@users.noreply.github.com>
2026-05-23 19:42:38 +0500

    Fix Android rust errors: macro syntax and type casts

--- rust/src/android_support.rs
+++ rust/src/android_support.rs
@@ -3,7 +3,7 @@
 use jni::objects::JObject;
 use jni::JNIEnv;
 
-wry::android_binding!(com_hussnain5455, godot_wry);
+wry::android_binding! { com_hussnain5455, godot_wry }
 
 #[no_mangle]
 pub unsafe extern "system" fn Java_com_hussnain5455_godot_1wry_WryActivity_initializeWry(
@@ -14,10 +14,10 @@ pub unsafe extern "system" fn Java_com_hussnain5455_godot_1wry_WryActivity_initi
     let vm_ptr = jvm.get_java_vm_pointer();
     let activity_raw = activity.as_raw();
     
-    ndk_context::initialize_android_context(vm_ptr, activity_raw as *mut _);
+    ndk_context::initialize_android_context(vm_ptr as *mut std::ffi::c_void, activity_raw as *mut std::ffi::c_void);
     
-    let looper = ndk::looper::ThreadLooper::for_thread()
-        .unwrap_or_else(|| ndk::looper::ThreadLooper::prepare());
+    let looper = wry::prelude::ndk::looper::ThreadLooper::for_thread()
+        .unwrap_or_else(|| wry::prelude::ndk::looper::ThreadLooper::prepare());
         
     let activity_ref = env.new_global_ref(&activity).expect("Failed to create GlobalRef");
     
